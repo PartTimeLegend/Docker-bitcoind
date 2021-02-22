8fe433b91d22fb3f9ac48371e18bd6e4a1847e59
Antony Bailey <support@antonybailey.net>
2021-02-22 09:05:28 +0000

    No wallets please

--- cat.sh
+++ cat.sh
@@ -2,6 +2,7 @@
 cat <<EOF > /etc/bitcoin.conf
 datadir=/bitcoin
 printtoconsole=1
+disablewallet=1
 rpcuser=${RPCUSER:-bitcoinrpc}
 rpcpassword=${RPCPASSWORD:-`dd if=/dev/urandom bs=33 count=1 2>/dev/null | base64`}
 EOF
